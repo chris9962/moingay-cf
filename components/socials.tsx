7078777ac86b37a6b3c5831826c45bbecaf6857d
Long <chuyentina2@gmail.com>
2025-06-17 12:30:55 +0700

    Replace Zalo icon with provided SVG

--- components/socials.tsx
+++ components/socials.tsx
@@ -2,23 +2,20 @@ import { Facebook, Instagram } from "lucide-react"
 import type { SVGProps } from "react"
 
 const ZaloIcon = (props: SVGProps<SVGSVGElement>) => (
-    <text
-      x="12"
-      y="13"
-      fontSize="6"
-      textAnchor="middle"
-      fontWeight="bold"
-      dominantBaseline="middle"
-    >
-      Zalo
-    </text>
-)
-
-export default function Socials() {
-  return (
-    <div className="fixed bottom-20 right-4 z-50 flex flex-col items-center space-y-2">
-      <a
-        href="#"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    id="svg"
+    version="1.1"
+    width="400"
+    height="400"
+    viewBox="0 0 400 400"
+    <g id="svgg">
+      <path id="path0" d="" stroke="none" fill="#040404" fillRule="evenodd" />
+      <path id="path1" d="" stroke="none" fill="#080404" fillRule="evenodd" />
+      <path id="path2" d="" stroke="none" fill="#080404" fillRule="evenodd" />
+      <path id="path3" d="" stroke="none" fill="#080404" fillRule="evenodd" />
+      <path id="path4" d="" stroke="none" fill="#080404" fillRule="evenodd" />
+    </g>
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Facebook"
